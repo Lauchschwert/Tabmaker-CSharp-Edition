b39e6524b39c7e8bc29cf7c74021fbd3855b113d
Lauchschwert <niclas.arnoldner@gmail.com>
2024-11-04 19:43:30 +0100

    Projektdateien hinzufügen.

---
+++ Tabmaker C# Edition/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Tabmaker_C__Edition
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
